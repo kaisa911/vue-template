0056067509f5193821c5059370fb1634644e5039
bufan.li <lbf911@126.com>
2019-10-16 13:36:42 +0800

    添加组件

--- src/views/index.vue
+++ src/views/index.vue
@@ -8,7 +8,7 @@
 export default {
   name: 'Index',
   components: {
-    HelloVue: () => import('../components/helloVue.vue'),
+    HelloVue: () => import('../components/HelloVue/helloVue.vue'),
   },
 };
 </script>
